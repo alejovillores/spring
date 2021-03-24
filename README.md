e1792ee697e4e901e16ac32696225e6bfcb6c119
Alejo <alejovillores@gmail.com>
2021-03-24 13:46:59 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# spring
+## Aprendiendo Spring
+[Documentación oficial](https://spring.io/)
+
+#### Temario
+* Controllers
+* Servicios
+* Modelos
+* Dependency Injection
+* @Transient
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,12 @@
+package com.example.demo.student;
+import org.springframework.stereotype.Service;
+
+
+/*
+ * El Service es que maneja la logíca de la app
+ * No es el contoller, es decir, no MAPEA peticiones.
+ * */
+
+@Service
+public class StudentService {
+}
