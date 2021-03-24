ff89f1f3a0044270296b9efa3b3c8f303ea6a3e4
Alejo <alejovillores@gmail.com>
2021-03-24 13:58:15 -0300

    first look

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,20 @@
 package com.example.demo.student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
+/*
+* El RestController  tells Spring that this code describes an endpoint
+* that should be made available over the web. Es decir, le dice al framework
+* que describe un punto final disponible para la web
+*
+* */
+@RestController
 public class StudentController {
+
+    //GetMapping mapea aquellos GET request.
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
